7e74d87bb8ba3c1002ab1ce3d6f6e42642023a3f
IskandarNabiev <Lampard.github>
2022-01-28 18:08:37 +0300

    HW-lesson8

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,14 +1,25 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class RemoveElementWithoutErrorsTest {
+
+public class RemoveElementWithoutErrorsTest{
+
+    public List<Integer> list;
+
+
+    @BeforeEach
+    public void initEach() {
+        list = new ArrayList<>(Arrays.asList(1, 2, 3));
+    }
+
 
     @Test
     public void successConcurrentModificationException() {
@@ -26,11 +37,56 @@ public class RemoveElementWithoutErrorsTest {
             }
             
         });
-
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
+
+        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
+                Integer value = iterator.next();
+                if (value == 2) {
+                    iterator.remove();
+                }
+            }
+
+        assertFalse(list.contains(2));
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors2() {
+
+        List<Integer> newList = new ArrayList<>();
+
+        for (Integer value : list) {
+            if (value == 3) {
+                newList.add(value);
+            }
+        }
+
+        list.removeAll(newList);
+        assertFalse(list.contains(3));
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors3() {
+
+        list.removeIf(value -> value == 1);
+        assertFalse(list.contains(1));
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors4() {
+
+        List<String> collected = list.stream()
+                .filter(value -> value != 2)
+                .map(Object::toString)
+                .collect(toList());
+
+        assertFalse(collected.contains("2"));
+        assertTrue(collected.contains("1"));
+        assertTrue(collected.contains("3"));
 
     }
 
