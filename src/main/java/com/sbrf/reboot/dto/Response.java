7ee8a67a1d741c70f2856147dc02f725818f1f81
IskandarNabiev <Lampard.github>
2022-02-02 18:50:53 +0300

    HW-lesson9

---
+++ src/main/java/com/sbrf/reboot/dto/Response.java
@@ -0,0 +1,15 @@
+package com.sbrf.reboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response extends Request {
+
+    private String statusCode;
+
+}
